cf4dd95a1a2f01ae96a2c1e9f03465add5bb553b
samettunay <samet.tunay12@gmail.com>
2025-03-18 23:52:51 +0300

    Add search component to right sidebar

--- src/layouts/main/rightbar/index.jsx
+++ src/layouts/main/rightbar/index.jsx
@@ -1,6 +1,9 @@
+import Search from "./search";
+
 export default function RightBar() {
     return (
         <aside className="w-[350px] mr-2.5">
+            <Search />
             sag sidebar!
         </aside>
     )
---
+++ src/layouts/main/rightbar/search/index.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+
+export default function Search() {
+    const [query, setQuery] = useState('')
+
+    return (
+        <div className="min-h-[32px] h-[52px] mb-3 flex items-center">
+            <label className="h-[43px] rounded-full w-full group relative border border-[#333639] focus-within:border-[#1d9bf0] focus-within:border-2 transition-all duration-200">
+                <div className="w-[28px] h-[43px] pl-3 flex items-center justify-center absolute top-0 left-0">
+                    <svg
+                        viewBox="0 0 24 24"
+                        height={16}
+                        className="min-w-[32px] text-[#71767b]">
+                        <path fill="currentColor"
+                            d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z" />
+                    </svg>
+                </div>
+                <input
+                    type="text"
+                    placeholder="Search"
+                    className="w-full h-full rounded-full outline-none pl-[32px] bg-transparent text-[15px]"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                {query && (
+                    <button
+                        type="button"
+                        onClick={() => setQuery('')}
+                        className="w-[22px] h-[22px] rounded-full flex items-center justify-center min-w-[22px] absolute top-1/2 -translate-y-1/2 right-3">
+                        <svg viewBox="0 0 24 24" width={22} height={22}>
+                            <path
+                                fill="currentColor"
+                                d="M12 1.75C6.34 1.75 1.75 6.34 1.75 12S6.34 22.25 12 22.25 22.25 17.66 22.25 12 17.66 1.75 12 1.75zm3.71 12.54l-1.42 1.42-2.29-2.3-2.29 2.3-1.42-1.42 2.3-2.29-2.3-2.29 1.42-1.42 2.29 2.3 2.29-2.3 1.42 1.42-2.3 2.29 2.3 2.29z"></path>
+                        </svg>
+                    </button>
+                )}
+            </label>
+        </div>
+    )
+}
