e8b1470e1ecd6566d546730748e7b5ce885c690d
samettunay <samet.tunay12@gmail.com>
2025-03-19 03:20:22 +0300

    Update sidebar layout

--- src/layouts/main/sidebar/index.jsx
+++ src/layouts/main/sidebar/index.jsx
@@ -4,7 +4,7 @@ import Account from "./account";
 
 export default function SideBar() {
     return (
-        <aside className="w-[275px] min-h-screen px-2 flex flex-col">
+        <aside className="w-[275px] max-h-screen min-h-screen px-2 flex flex-col sticky z-[2] top-0">
             <Logo />
             <Menu />
 
