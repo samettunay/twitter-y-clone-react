db9dcbf172492afa63be9cd5b8aad338c9475227
samettunay <samet.tunay12@gmail.com>
2025-03-22 16:52:40 +0300

    Enhance appearance settings by adding alpha values for background colors, update Tailwind CSS imports, and restructure Home component with tabs for better navigation.

--- src/modals/appearance/index.jsx
+++ src/modals/appearance/index.jsx
@@ -126,6 +126,7 @@ export default function AppearanceModal({ close }) {
                                     setBackgroundColor({
                                         name: 'light',
                                         primary: '#ffffff',
+                                        primaryAlpha: '#ffffffa6',
                                         secondary: '#f7f9f9',
                                         third: '#eff3f4',
                                         modal: '#00000066',
@@ -167,6 +168,7 @@ export default function AppearanceModal({ close }) {
                                     setBackgroundColor({
                                         name: 'dark',
                                         primary: '#15202b',
+                                        primaryAlpha: '#15202ba6',
                                         secondary: '#1e2732',
                                         third: '#263340',
                                         modal: '#5b708366',
@@ -208,6 +210,7 @@ export default function AppearanceModal({ close }) {
                                     setBackgroundColor({
                                         name: 'darker',
                                         primary: '#000000',
+                                        primaryAlpha: '#000000a6',
                                         secondary: '#16181c',
                                         third: '#212327',
                                         modal: '#5b708366',
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,32 @@
 import { Helmet } from 'react-helmet'
+import Tab from '~/components/tab'
+import StickyHeader from '~/components/sticky-header'
 
 export default function Home() {
     return (
-        <div>
+        <>
             <Helmet>
                 <title>Home / Y</title>
             </Helmet>
-            Home Component!
-        </div>
+            <Tab activeTab="followings">
+				<StickyHeader>
+					<Tab.Items>
+						<Tab.Item id="for-you">
+							For You
+						</Tab.Item>
+						<Tab.Item id="followings">
+							Following
+						</Tab.Item>
+					</Tab.Items>
+				</StickyHeader>
+				<Tab.Content id="for-you">
+					1. content
+				</Tab.Content>
+				<Tab.Content id="followings">
+					2. content
+				</Tab.Content>
+			</Tab>
+        </>
     )
 
 }
